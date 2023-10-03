31a3577b33a77d8b733d26c99a4fe2b543061313
psyang <12964188+psyang@user.noreply.gitee.com>
2023-10-03 23:55:00 +0800

    answer 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,8 @@
+const CourseArray = ({ courses }) => {
+  return (courses.map((course) => < Course course={course} key={course.id}/>))
+}
+
+
 const Course = ({ course }) => {
   let courseInfo = course.parts.map((part) => <p key={part.id}>{part.name} {part.exercises}</p>)
   let total = course.parts.reduce((sum, part) => (sum + part.exercises), 0)
@@ -5,35 +10,58 @@ const Course = ({ course }) => {
     <div>
       <h1>{course.name}</h1>
       {courseInfo}
-      <p>total of {total} exercises</p>
+      <b>total of {total} exercises</b>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <CourseArray courses={courses} />
 }
 
 export default App
