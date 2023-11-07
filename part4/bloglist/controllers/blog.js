06aab050dd409e4ecb08dd95e540eecb536dcfaa
psyang <61995003+peisong13@users.noreply.github.com>
2023-11-07 22:31:53 +0800

    answer 4.17

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,27 +1,35 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
 })
   
-blogRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogRouter.post('/', async (request, response) => {
+    const body = request.body
+    const user = await User.findOne()
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
 
     if (!blog['title'] || !blog['url']) {
         response.status(400).json({ error: 'content missing' })
         return
     }
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const savedBlog = await blog.save()
+    
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
 })
 
 blogRouter.delete('/:id', (request, response) => {
--- part4/bloglist/controllers/user.js
+++ part4/bloglist/controllers/user.js
@@ -41,7 +41,8 @@ userRouter.post('/', async (request, response) => {
 })
 
 userRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    // populate user with blog, turn off strictPopulate, select url
+    const users = await User.find({}).populate({path: 'blogs', select: { url: 1, title: 1, author: 1 }})
     response.json(users)
 })
 
